75b170661f9d989b5b0f879f6fd374bf52820924
John Kyeyune <genesint@gmail.com>
2015-06-10 12:48:44 +0300

    Setup website structure

---
+++ sites/all/modules/custom/customui/views/about-us.tpl.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-8"></div>
+    <div class="col-md-1"></div>
+</div>
+
+<div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-8"></div>
+    <div class="col-md-1"></div>
+</div>
+
+<div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-8"></div>
+    <div class="col-md-1"></div>
+</div>
+
+<div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-8"></div>
+    <div class="col-md-1"></div>
+</div>
